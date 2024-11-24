8f8787d520d542e8447a751378086b4b8ad31043
ashwinth2312 <ashwinthkumar003@gmail.com>
2024-11-24 20:52:56 +0530

    Update ecom frontend.js

--- ecom frontend.js
+++ ecom frontend.js
@@ -1,5 +1,5 @@
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
 
 function ProductList() {
   const [products, setProducts] = useState([]);
@@ -13,15 +13,23 @@ function ProductList() {
     fetchProducts();
   }, []);
 
-  const addToCart = (productId) => {
-    // Send a POST request to the backend API to add the product to the cart
-    axios.post('/cart', { productId, quantity: 1 })
-      .then(response => {
-        setCart(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+  const addToCart = async (productId) => {
+    try {
+      const response = await axios.post('/cart', { productId, quantity: 1 });
+      setCart(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const removeFromCart = async (productId) => {
+    // Implement logic to remove an item from the cart
+    // You can send a DELETE request to the backend API
+  };
+
+  const updateCartItem = async (productId, newQuantity) => {
+    // Implement logic to update the quantity of an item in the cart
+    // You can send a PUT request to the backend API
   };
 
   return (
@@ -41,6 +49,8 @@ function ProductList() {
         {cart.map(item => (
           <li key={item.productId}>
             {products.find(p => p.id === item.productId).name} x {item.quantity}
+            <button onClick={() => removeFromCart(item.productId)}>Remove</button>
+            <input type="number" value={item.quantity} onChange={(e) => updateCartItem(item.productId, e.target.value)} />
           </li>
         ))}
       </ul>
